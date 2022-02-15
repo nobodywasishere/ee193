0f4b18fc6061de3ed731a1d7192177e7ff168fd8
Michael Riegert <michael@eowyn.net>
2022-02-15 17:11:48 -0500

    Correctly working blink for stm32cube and cmsis frameworks

--- 02-blinky/cmsis/src/main.c
+++ 02-blinky/cmsis/src/main.c
@@ -1,4 +1,29 @@
+// https://github.com/platformio/platform-ststm32/blob/develop/examples/cmsis-blink/src/main.c
 
-int main(void) {
+#include "stm32wlxx.h"
+#define LEDPORT (GPIOB)
+#define LED1 (5)
+#define ENABLE_GPIO_CLOCK (RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN)
+#define _MODER    MODER
+#define GPIOMODER (GPIO_MODER_MODE5_0)
+
+void ms_delay(int ms)
+{
+   while (ms-- > 0) {
+      volatile int x=500;
+      while (x-- > 0)
+         __asm("nop");
+   }
+}
+
+//Alternates blue and green LEDs quickly
+int main(void)
+{
+    ENABLE_GPIO_CLOCK;              // enable the clock to GPIO
+    LEDPORT->_MODER |= GPIOMODER;   // set pins to be general purpose output
+    for (;;) {
+        ms_delay(500);
+        LEDPORT->ODR ^= (1<<LED1);  // toggle diodes
+    }
     return 0;
 }
--- 02-blinky/stm32cube/src/main.c
+++ 02-blinky/stm32cube/src/main.c
@@ -3,8 +3,8 @@
 #include "stm32wlxx_hal.h"
 
 #define LED_PIN                                GPIO_PIN_5
-#define LED_GPIO_PORT                          GPIOA
-#define LED_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOA_CLK_ENABLE()
+#define LED_GPIO_PORT                          GPIOB
+#define LED_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOB_CLK_ENABLE()
 
 int main(void)
 {
@@ -24,7 +24,7 @@ int main(void)
   {
     HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
     
-    HAL_Delay(1000);
+    HAL_Delay(100);
   }
 }
 
